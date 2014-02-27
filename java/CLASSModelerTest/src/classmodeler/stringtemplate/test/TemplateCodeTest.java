23a9c1cd06b61bce504c81db2a1885e1ca235334
Gabriel Leonardo Diaz <gabriel.leonardo.diaz@gmail.com>
2014-02-26 23:36:16 -0500

    Kick off StringTemplate library.

--- java/CLASSModelerTest/src/classmodeler/stringtemplate/test/TemplateCodeTest.java
+++ java/CLASSModelerTest/src/classmodeler/stringtemplate/test/TemplateCodeTest.java
@@ -8,7 +8,13 @@
 
 package classmodeler.stringtemplate.test;
 
+import java.io.StringReader;
+
+import org.antlr.stringtemplate.StringTemplate;
+import org.antlr.stringtemplate.StringTemplateGroup;
 import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupFile;
 import org.testng.annotations.Test;
 
 /**
@@ -20,9 +26,19 @@ public class TemplateCodeTest {
   
   @Test
   public void generateCodeTemplate () {
-    ST hello = new ST("Hello, <name>");
-    hello.add("name", "World");
-    System.out.println(hello.render());
+    String templates = "group simple; vardef(type,name) ::= \"<type> <name>;\""; // templates from above
+    // Use the constructor that accepts a Reader
+    StringTemplateGroup group = new StringTemplateGroup(new StringReader(templates));
+    StringTemplate t = group.getInstanceOf("vardef");
+    t.setAttribute("type", "int");
+    t.setAttribute("name", "foo");
+    System.out.println(t);
+    
+    STGroup g = new STGroupFile("classmodeler/stringtemplate/test/ClassTemplate.stg");
+    ST st = g.getInstanceOf("class");
+    st.add("visibility", "protected");
+    st.add("name", "Person");
+    System.out.println(st.render());
   }
   
 }
